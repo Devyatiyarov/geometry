c0ba75ccea1f1f0eeddd76aed631bf9ba95fc531
Devytiyarov <devytiyarov00@bk.ru>
2019-04-11 23:12:04 +0700

    add name new_funcs

--- src/circle.h
+++ src/circle.h
@@ -7,5 +7,8 @@ typedef struct {
 	int index;
 } Circle;
 
+void get_intersect(Circle *c, int size, int index);
+double get_perimeter(Circle *c, int i);
+double get_area(Circle *c, int i);
 void print_circle(Circle *c, int i);
 int scan_errors(Circle *c, char *str, int index, int count_err);
