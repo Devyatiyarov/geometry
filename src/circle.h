7dfb155514373ac6923af04b0c055ded66da085b
Devyatiyarov <devyatiyarov00@bk.ru>
2019-04-12 16:15:44 +0700

    clang on cir.h

--- src/circle.h
+++ src/circle.h
@@ -1,14 +1,14 @@
 #define MAX 100
 
 typedef struct {
-	double x;
-	double y;
-	double r;
-	int index;
+    double x;
+    double y;
+    double r;
+    int index;
 } Circle;
 
-//void get_intersect(Circle *c, int size, int index);
-double get_perimeter(Circle *c, int i);
-double get_area(Circle *c, int i);
-void print_circle(Circle *c, int i);
-int scan_errors(Circle *c, char *str, int index, int count_err);
+// void get_intersect(Circle *c, int size, int index);
+double get_perimeter(Circle* c, int i);
+double get_area(Circle* c, int i);
+void print_circle(Circle* c, int i);
+int scan_errors(Circle* c, char* str, int index, int count_err);
