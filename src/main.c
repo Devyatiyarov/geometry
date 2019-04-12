732d8a03c05a8768b733f33516586fd2cd36e49c
Devyatiyarov <devyatiyarov00@bk.ru>
2019-04-12 16:15:14 +0700

    clang on main

--- src/main.c
+++ src/main.c
@@ -1,36 +1,36 @@
+#include "circle.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "circle.h"
 
 int main()
 {
-	FILE *file;
-        file = fopen("c.txt", "r");
-        if (!file) {
-        	printf("Can`t open file\n");
-        	return -1;
-   	}
-	char str[MAX];
-	Circle *c = (Circle *)malloc(sizeof(Circle) * MAX);
-	int i = 0;
-	int count_err = 0;
-    	while (!feof(file)) {
-    		i++;
-    		(c + i)->index = i;
-       		fgets(str, MAX, file);
-        	if (scan_errors(c, str, i, count_err) == -1) {
-        		i--;
-        		count_err++;
-    			continue;
-        	}
-      	}
+    FILE* file;
+    file = fopen("c.txt", "r");
+    if (!file) {
+        printf("Can`t open file\n");
+        return -1;
+    }
+    char str[MAX];
+    Circle* c = (Circle*)malloc(sizeof(Circle) * MAX);
+    int i = 0;
+    int count_err = 0;
+    while (!feof(file)) {
+        i++;
+        (c + i)->index = i;
+        fgets(str, MAX, file);
+        if (scan_errors(c, str, i, count_err) == -1) {
+            i--;
+            count_err++;
+            continue;
+        }
+    }
 
-    	print_circle(c, i);
+    print_circle(c, i);
 
-    	fclose(file);
-    	free(c);
-    	c = NULL;
-    
-	return 0;
+    fclose(file);
+    free(c);
+    c = NULL;
+
+    return 0;
 }
