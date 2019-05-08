08446a2050dac192aebd232bdb0c01f29122575d
Devyatiyarov <devyatiyarov00@bk.ru>
2019-05-08 23:23:55 +0700

    Upgrade main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "circle.h"
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     FILE* file;
-    file = fopen("c.txt", "r");
+    file = fopen("./input/c.txt", "r");
     if (!file) {
         printf("Can`t open file\n");
         return -1;
@@ -15,11 +15,18 @@ int main()
     Circle* c = (Circle*)malloc(sizeof(Circle) * MAX);
     int i = 0;
     int count_err = 0;
+    int check_ret;
     while (!feof(file)) {
+        fgets(str, MAX, file);
+        if (strcmp(str, "\n\0") == 0) {
+            continue;
+        }
         i++;
         (c + i)->index = i;
-        fgets(str, MAX, file);
-        if (scan_errors(c, str, i, count_err) == -1) {
+        check_ret = scan_errors(c, str, i);
+        if (check_ret == 1 || check_ret == 2 || check_ret == 3 || check_ret == 4
+            || check_ret == 5) {
+            print_error(check_ret, i, count_err);
             i--;
             count_err++;
             continue;
